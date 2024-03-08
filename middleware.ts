5cbc9e7ddbd2fb3f4acef6f140b58cfc30f1161e
Manuel Almaguer <manunoly@gmail.com>
2024-03-07 19:51:21 -0500

    some fix to compile

--- middleware.ts
+++ middleware.ts
@@ -10,7 +10,7 @@ import {
 
 const { auth } = NextAuth(authConfig);
 
-export default auth((req) => {
+export default auth((req): void | Response | Promise<void | Response> => {
   const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
 
@@ -19,14 +19,14 @@ export default auth((req) => {
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
   if (isApiAuthRoute) {
-    return null;
+    return;
   }
 
   if (isAuthRoute) {
     if (isLoggedIn) {
       return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
     }
-    return null;
+    return;
   }
 
   if (!isLoggedIn && !isPublicRoute) {
@@ -43,7 +43,7 @@ export default auth((req) => {
     ));
   }
 
-  return null;
+  return;
 })
 
 // Optionally, don't invoke Middleware on some paths
